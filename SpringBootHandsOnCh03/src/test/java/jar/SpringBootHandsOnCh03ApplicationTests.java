eb9f8b9736243f48d5d04a31f04c14f063a9138d
takahashi <takahashi@192.168.0.5>
2016-02-14 16:51:04 +0900

    プロジェクトを新規作成

---
+++ SpringBootHandsOnCh03/src/test/java/jar/SpringBootHandsOnCh03ApplicationTests.java
@@ -0,0 +1,16 @@
+package jar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootHandsOnCh03Application.class)
+public class SpringBootHandsOnCh03ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
